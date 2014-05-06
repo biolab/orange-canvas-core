aa177e14df9978b3bea3edd272b4cc5d9a4ead2b
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2014-05-06 15:01:09 +0200

    Changed "Documentation" link in the welcome dialog.
    Transplanted-from: biolab/orange@1ef610de731d1af471f0c3ea741eb4f4fbde9cd1

--- OrangeCanvas/application/canvasmain.py
+++ OrangeCanvas/application/canvasmain.py
@@ -1242,7 +1242,7 @@ class CanvasMainWindow(QMainWindow):
     def documentation(self, *args):
         """Show reference documentation.
         """
-        url = QUrl(LINKS["tutorial"])
+        url = QUrl(LINKS["start-using"])
         QDesktopServices.openUrl(url)
 
     def recent_scheme(self, *args):
