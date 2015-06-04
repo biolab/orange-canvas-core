442c5c1ad4a7a6f95374cf04ac10c12ceb05f8e1
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-06-04 10:05:39 +0200

    Fix preview browser tests.

--- OrangeCanvas/preview/tests/test_previewbrowser.py
+++ OrangeCanvas/preview/tests/test_previewbrowser.py
@@ -8,20 +8,23 @@ from ...gui import test
 
 from ..previewbrowser import PreviewBrowser
 from ..previewmodel import PreviewItem, PreviewModel
+from ... import config
 
 import pkg_resources
 
-svg1 = pkg_resources.resource_string("Orange.OrangeCanvas",
+svg1 = pkg_resources.resource_string(config.__package__,
                                      "icons/default-category.svg")
 
-svg2 = pkg_resources.resource_string("Orange.OrangeCanvas",
+svg2 = pkg_resources.resource_string(config.__package__,
                                      "icons/default-widget.svg")
 
 
 def construct_test_preview_model():
-    items = [("Name1", "A preview item 1", svg1, u"~/bla", ),
-             ("Name2", "A preview item 2" + "long text" * 5, svg2, "~/item")
-             ]
+    items = [
+        ("Name1", "A preview item 1", svg1.decode("utf-8"), u"~/bla", ),
+        ("Name2", "A preview item 2" + "long text" * 5,
+         svg2.decode("utf-8"), "~/item")
+    ]
 
     items = [PreviewItem(*arg[:-1], path=arg[-1]) for arg in items]
     model = PreviewModel(items=items)
