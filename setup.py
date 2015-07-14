fff01033857df15aaff403de518054de94aaa964
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-07-14 11:37:05 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 from setuptools import setup, find_packages
 
 NAME = "Orange-Canvas-Core"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = "Core component of Orange Canvas"
 LONG_DESCRIPTION = open("README.txt", "rt").read()
 
