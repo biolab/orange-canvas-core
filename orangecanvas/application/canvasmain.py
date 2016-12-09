07f8eaedb41e4a0308eb941800ec028d3e23466e
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2016-12-09 19:59:06 +0100

    Remove 'env' parameter from the workflow constructor call
    Preserve call compatibility with custom workflow subclasses which
    do not pass **kwargs to its super class.

--- orangecanvas/application/canvasmain.py
+++ orangecanvas/application/canvasmain.py
@@ -973,8 +973,8 @@ class CanvasMainWindow(QMainWindow):
         from a saved `filename`. Return `None` if an error occurs.
 
         """
-        new_scheme = config.workflow_constructor(
-            parent=self, env={"basedir": os.path.dirname(filename)})
+        new_scheme = config.workflow_constructor(parent=self)
+        new_scheme.set_runtime_env("basedir", os.path.dirname(filename))
         errors = []
         try:
             scheme_load(new_scheme, open(filename, "rb"),
