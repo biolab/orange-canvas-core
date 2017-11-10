51312b3237d38da300400e633246953d27e7c0c3
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-11-10 16:15:06 +0100

    styles: Add an inverted color theme

--- orangecanvas/styles/__init__.py
+++ orangecanvas/styles/__init__.py
@@ -1,10 +1,13 @@
 """
 QSS style sheets.
 """
+from typing import Mapping, Callable
+
+from AnyQt.QtCore import Qt
 from AnyQt.QtGui import QPalette, QColor
 
 
-def breeze_dark():
+def breeze_dark() -> QPalette:
     # 'Breeze Dark' color scheme from KDE.
     text = QColor(239, 240, 241)
     textdisabled = QColor(98, 108, 118)
@@ -40,3 +43,51 @@ def breeze_dark():
     palette.setColor(QPalette.Shadow, shadow)
     palette.setColor(QPalette.Link, link)
     return palette
+
+
+def zion_reversed() -> QPalette:
+    # 'Zion Reversed' color scheme from KDE.
+    text = Qt.white
+    textdisabled = QColor(85, 85, 85)
+    window = QColor(16, 16, 16)
+    base = Qt.black
+    highlight = QColor(0, 49, 110)
+    highlight_disabled = window
+    link = QColor(128, 181, 255)
+
+    light = QColor(174, 174, 174)
+    mid = QColor(89, 89, 89)
+    dark = QColor(118, 118, 118)
+    shadow = QColor(141, 141, 141)
+
+    palette = QPalette()
+    palette.setColor(QPalette.Window, window)
+    palette.setColor(QPalette.WindowText, text)
+    palette.setColor(QPalette.Disabled, QPalette.WindowText, textdisabled)
+    palette.setColor(QPalette.Base, base)
+    palette.setColor(QPalette.AlternateBase, window)
+    palette.setColor(QPalette.Disabled, QPalette.Base, window)
+    palette.setColor(QPalette.Disabled, QPalette.AlternateBase, window)
+    palette.setColor(QPalette.ToolTipBase, window)
+    palette.setColor(QPalette.ToolTipText, text)
+    palette.setColor(QPalette.Text, text)
+    palette.setColor(QPalette.Disabled, QPalette.Text, textdisabled)
+    palette.setColor(QPalette.Button, window)
+    palette.setColor(QPalette.ButtonText, text)
+    palette.setColor(QPalette.Disabled, QPalette.ButtonText, textdisabled)
+    palette.setColor(QPalette.BrightText, Qt.white)
+    palette.setColor(QPalette.Highlight, highlight)
+    palette.setColor(QPalette.Disabled, QPalette.Highlight, highlight_disabled)
+    palette.setColor(QPalette.HighlightedText, text)
+    palette.setColor(QPalette.Light, light)
+    palette.setColor(QPalette.Mid, mid)
+    palette.setColor(QPalette.Dark, dark)
+    palette.setColor(QPalette.Shadow, shadow)
+    palette.setColor(QPalette.Link, link)
+    return palette
+
+
+colorthemes = {
+    "breeze-dark": breeze_dark,
+    "dark": zion_reversed
+}  # type: Mapping[str, Callable[[],QPalette]]
