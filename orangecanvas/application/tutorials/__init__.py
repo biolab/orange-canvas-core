194eb41f4e36edc7926ae3eb78cc054c18a230f6
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-11-23 18:20:54 +0100

    Fix example workflow discovery
    Use configured entry point

--- orangecanvas/application/tutorials/__init__.py
+++ orangecanvas/application/tutorials/__init__.py
@@ -3,55 +3,42 @@ Orange Canvas Tutorial schemes
 
 """
 import os
-import io
 import logging
 import types
-import collections
-from itertools import chain
 
 import six
-
 import pkg_resources
+
+from orangecanvas import config
 
 log = logging.getLogger(__name__)
 
 
-def list_schemes(package):
-    """Return a list of scheme tutorials.
+def list_workflows(package):
+    # type: (types.ModuleType) -> List[str]
     """
+    Return a list of .ows files in the located next to `package`.
+    """
+
+    def is_ows(filename):
+        return filename.endswith(".ows")
+
     resources = pkg_resources.resource_listdir(package.__name__, ".")
     resources = filter(is_ows, resources)
     return sorted(resources)
 
 
-def is_ows(filename):
-    return filename.endswith(".ows")
-
-
-def default_entry_point():
-    dist = pkg_resources.get_distribution("Orange")
-    ep = pkg_resources.EntryPoint("Orange Canvas", __name__, dist=dist)
-    return ep
-
-
-def tutorial_entry_points():
-    """Return an iterator over all tutorials.
-    """
-    default = default_entry_point()
-    return chain([default],
-                 pkg_resources.iter_entry_points("orange.widgets.tutorials"))
-
-
 def tutorials():
-    """Return all known tutorials.
+    """
+    Return all known example workflows.
     """
     all_tutorials = []
-    for ep in tutorial_entry_points():
+    for ep in config.default.tutorials_entry_points():
         tutorials = None
         try:
             tutorials = ep.resolve()
         except pkg_resources.DistributionNotFound as ex:
-            log.warning("Could not load tutorials from %r (%r)",
+            log.warning("Could not load examples from %r (%r)",
                         ep.dist, ex)
             continue
         except ImportError:
@@ -65,8 +52,9 @@ def tutorials():
 
         if isinstance(tutorials, types.ModuleType):
             package = tutorials
-            tutorials = list_schemes(tutorials)
-            tutorials = [Tutorial(t, package, ep.dist) for t in tutorials]
+            tutorials = list_workflows(tutorials)
+            tutorials = [ExampleWorkflow(t, package, ep.dist)
+                         for t in tutorials]
         elif isinstance(tutorials, (types.FunctionType, types.MethodType)):
             try:
                 tutorials = tutorials()
@@ -75,23 +63,24 @@ def tutorials():
                           "unexpected error.",
                           ex, exc_info=True)
                 continue
-            tutorials = [Tutorial(t, package=None, distribution=ep.dist)]
+            tutorials = [ExampleWorkflow(t, package=None, distribution=ep.dist)
+                         for t in tutorials]
 
         all_tutorials.extend(tutorials)
 
     return all_tutorials
 
 
-class Tutorial(object):
+class ExampleWorkflow(object):
     def __init__(self, resource, package=None, distribution=None):
         self.resource = resource
         self.package = package
         self.distribution = distribution
 
     def abspath(self):
-        """Return absolute filename for the scheme if possible else
+        """
+        Return absolute filename for the workflow if possible else
         raise an ValueError.
-
         """
         if self.package is not None:
             return pkg_resources.resource_filename(self.package.__name__,
