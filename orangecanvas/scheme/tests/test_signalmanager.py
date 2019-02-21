8aa967481e837b71a2dea9a661249d7daf1f0a5c
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-02-21 12:36:52 +0100

    tests/signalmanager: Add tests for signal propagation

--- orangecanvas/scheme/tests/test_signalmanager.py
+++ orangecanvas/scheme/tests/test_signalmanager.py
@@ -1,6 +1,125 @@
 import unittest
 
-from .. import signalmanager
+from AnyQt.QtWidgets import QApplication
+from AnyQt.QtTest import QSignalSpy
+
+from orangecanvas.scheme import Scheme, SchemeNode
+from orangecanvas.scheme import signalmanager
+from orangecanvas.scheme.signalmanager import (
+    SignalManager, Signal,  compress_signals
+)
+from orangecanvas.registry import tests as registry_tests
+
+
+class TestingSignalManager(SignalManager):
+    def is_blocking(self, node):
+        return bool(node.property("-blocking"))
+
+    def send_to_node(self, node, signals):
+        for sig in signals:
+            name = sig.link.sink_channel.name
+            node.setProperty("-input-" + name, sig.value)
+
+        for out in node.description.outputs:
+            self.send(node, out, "hello", None)
+
+
+class TestSignalManager(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        app = QApplication.instance()
+        if app is None:
+            app = QApplication([])
+        cls.app = app
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.app
+
+    def setUp(self):
+        reg = registry_tests.small_testing_registry()
+        scheme = Scheme()
+        zero = scheme.new_node(reg.widget("zero"))
+        one = scheme.new_node(reg.widget("one"))
+        add = scheme.new_node(reg.widget("add"))
+        scheme.new_link(zero, "value", add, "left")
+        scheme.new_link(one, "value", add, "right")
+        self.scheme = scheme
+
+    def test(self):
+        workflow = self.scheme
+        sm = TestingSignalManager()
+        sm.set_workflow(workflow)
+        sm.set_workflow(workflow)
+        sm.set_workflow(None)
+        sm.set_workflow(workflow)
+        sm.start()
+
+        self.assertFalse(sm.has_pending())
+
+        sm.stop()
+        sm.pause()
+        sm.resume()
+        n0, n1, n3 = workflow.nodes
+
+        sm.send(n0, n0.description.outputs[0], 'hello', None)
+        sm.send(n1, n1.description.outputs[0], 'hello', None)
+        spy = QSignalSpy(sm.processingFinished[SchemeNode])
+        self.assertTrue(spy.wait())
+        self.assertSequenceEqual(list(spy), [[n3]])
+        self.assertEqual(n3.property("-input-left"), 'hello')
+        self.assertEqual(n3.property("-input-right"), 'hello')
+
+        self.assertFalse(sm.has_pending())
+        workflow.remove_link(workflow.links[0])
+        self.assertTrue(sm.has_pending())
+
+        spy = QSignalSpy(sm.processingFinished[SchemeNode])
+        self.assertTrue(spy.wait())
+
+        self.assertEqual(n3.property("-input-left"), None)
+        self.assertEqual(n3.property("-input-right"), 'hello')
+
+    def test_compress_signals(self):
+        workflow = self.scheme
+        link = workflow.links[0]
+        self.assertSequenceEqual(compress_signals([]), [])
+        signals_in = [
+            Signal(link, 1, None),
+            Signal(link, 3, None),
+            Signal(link, 2, None),
+        ]
+        self.assertSequenceEqual(
+            compress_signals(signals_in),
+            signals_in[-1:]
+        )
+        signals_in = [
+            Signal(link, None, None),
+            Signal(link, 3, None),
+            Signal(link, 2, None),
+        ]
+        self.assertSequenceEqual(
+            compress_signals(signals_in),
+            [signals_in[0], signals_in[-1]]
+        )
+        signals_in = [
+            Signal(link, None, 1),
+            Signal(link, 3, 1),
+            Signal(link, 2, 2),
+        ]
+        self.assertSequenceEqual(
+            compress_signals(signals_in),
+            signals_in,
+        )
+        signals_in = [
+            Signal(link, 1, 1),
+            Signal(link, None, 1),
+            Signal(link, 2, 2),
+        ]
+        self.assertSequenceEqual(
+            compress_signals(signals_in),
+            signals_in[1:],
+        )
 
 
 class TestSCC(unittest.TestCase):
