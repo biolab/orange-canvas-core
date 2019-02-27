biolab/orange-canvas-core

commit 7d980fef00eeaca82ff10c2d68168c9f3ddad2e8

Use super() where appropriate

orangecanvas/application/settings.py
@@ -35,7 +35,7 @@ class UserDefaultsPropertyBinding(AbstractBoundProperty):
 
     """
     def __init__(self, obj, propertyName, parent=None):
-        AbstractBoundProperty.__init__(self, obj, propertyName, parent)
+        super().__init__(obj, propertyName, parent)
 
         obj.installEventFilter(self)
 
@@ -50,7 +50,7 @@ class UserDefaultsPropertyBinding(AbstractBoundProperty):
                 event.key() == self.propertyName:
             self.notifyChanged()
 
-        return AbstractBoundProperty.eventFilter(self, obj, event)
+        return super().eventFilter(obj, event)
 
 
 class UserSettingsModel(QAbstractItemModel):
@@ -60,7 +60,7 @@ class UserSettingsModel(QAbstractItemModel):
 
     """
     def __init__(self, parent=None, settings=None):
-        QAbstractItemModel.__init__(self, parent)
+        super().__init__(parent)
 
         self.__settings = settings
         self.__headers = ["Name", "Status", "Type", "Value"]
@@ -103,7 +103,7 @@ class UserSettingsModel(QAbstractItemModel):
             if role == Qt.DisplayRole:
                 return self.__headers[section]
 
-        return QAbstractItemModel.headerData(self, section, orientation, role)
+        return super().headerData(section, orientation, role)
 
     def data(self, index, role=Qt.DisplayRole):
         if self._valid(index):
@@ -187,7 +187,7 @@ class UserSettingsDialog(QMainWindow):
     MAC_UNIFIED = True
 
     def __init__(self, parent=None, **kwargs):
-        QMainWindow.__init__(self, parent, **kwargs)
+        super().__init__(parent, **kwargs)
         self.setWindowFlags(Qt.Dialog)
         self.setWindowModality(Qt.ApplicationModal)
 
@@ -474,7 +474,7 @@ class UserSettingsDialog(QMainWindow):
         return status
 
     def hideEvent(self, event):
-        QMainWindow.hideEvent(self, event)
+        super().hideEvent(self, event)
         if self.__loop is not None:
             self.__loop.exit(0)
             self.__loop = None

orangecanvas/utils/settings.py
@@ -54,7 +54,7 @@ class SettingChangedEvent(QEvent):
         """
         Initialize the event instance
         """
-        QEvent.__init__(self, etype)
+        super().__init__(etype)
         self.__key = key
         self.__value = value
         self.__oldValue = oldValue
@@ -98,7 +98,7 @@ class Settings(QObject, MutableMapping, metaclass=QABCMeta):
     keyRemoved = Signal(str)
 
     def __init__(self, parent=None, defaults=(), path=None, store=None):
-        QObject.__init__(self, parent)
+        super().__init__(parent)
 
         if store is None:
             store = QSettings()
@@ -321,10 +321,10 @@ class Settings(QObject, MutableMapping, metaclass=QABCMeta):
         """
         Return a list over of all values in the settings.
         """
-        return MutableMapping.values(self)
+        return super().values(self)
 
     def customEvent(self, event):
-        QObject.customEvent(self, event)
+        super().customEvent(event)
 
         if isinstance(event, SettingChangedEvent):
             if event.type() == SettingChangedEvent.SettingChanged:

orangecanvas/application/aboutdialog.py
@@ -24,7 +24,7 @@ ABOUT_TEMPLATE = """\
 
 class AboutDialog(QDialog):
     def __init__(self, parent=None, **kwargs):
-        QDialog.__init__(self, parent, **kwargs)
+        super().__init__(parent, **kwargs)
 
         if sys.platform == "darwin":
             self.setAttribute(Qt.WA_MacSmallSize, True)

orangecanvas/application/addons.py
@@ -164,7 +164,7 @@ class AddonManagerWidget(QWidget):
     statechanged = Signal()
 
     def __init__(self, parent=None, **kwargs):
-        super(AddonManagerWidget, self).__init__(parent, **kwargs)
+        super().__init__(parent, **kwargs)
 
         #: list of Available | Installed
         self.__items = []
@@ -428,7 +428,7 @@ def method_queued(method, sig, conntype=Qt.QueuedConnection):
 
 class AddonManagerDialog(QDialog):
     def __init__(self, parent=None, **kwargs):
-        super(AddonManagerDialog, self).__init__(parent, **kwargs)
+        super().__init__(parent, **kwargs)
         self.setLayout(QVBoxLayout())
 
         self.addonwidget = AddonManagerWidget()
@@ -560,13 +560,13 @@ class AddonManagerDialog(QDialog):
         return self.__progress
 
     def done(self, retcode):
-        super(AddonManagerDialog, self).done(retcode)
+        super().done(retcode)
         if self.__thread is not None:
             self.__thread.quit()
             self.__thread.wait(1000)
 
     def closeEvent(self, event):
-        super(AddonManagerDialog, self).closeEvent(event)
+        super().closeEvent(event)
         if self.__thread is not None:
             self.__thread.quit()
             self.__thread.wait(1000)
@@ -616,11 +616,11 @@ class AddonManagerDialog(QDialog):
 
 class SafeTransport(xmlrpc.client.SafeTransport):
     def __init__(self, use_datetime=0, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
-        super(SafeTransport, self).__init__(use_datetime)
+        super().__init__(use_datetime)
         self._timeout = timeout
 
     def make_connection(self, *args, **kwargs):
-        conn = super(SafeTransport, self).make_connection(*args, **kwargs)
+        conn = super().make_connection(*args, **kwargs)
         conn.timeout = self._timeout
         return conn
 
@@ -832,7 +832,7 @@ class Installer(QObject):
     error = Signal(str, object, int, list)
 
     def __init__(self, parent=None, steps=[]):
-        QObject.__init__(self, parent)
+        super().__init__(parent)
         self.__interupt = False
         self.__queue = deque(steps)
         self.__statusMessage = ""

orangecanvas/application/application.py
@@ -15,11 +15,10 @@ class CanvasApplication(QApplication):
         if hasattr(Qt, "AA_EnableHighDpiScaling"):
             # Turn on HighDPI support when available
             QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-        QApplication.__init__(self, argv)
+        super().__init__(argv)
         self.setAttribute(Qt.AA_DontShowIconsInMenus, True)
 
     def event(self, event):
         if event.type() == QEvent.FileOpen:
             self.fileOpenRequest.emit(event.url())
-
-        return QApplication.event(self, event)
+        return super().event(event)

orangecanvas/application/canvasmain.py
@@ -106,7 +106,7 @@ class FakeToolBar(QToolBar):
 
     """
     def __init__(self, *args, **kwargs):
-        QToolBar.__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.setFloatable(False)
         self.setMovable(False)
 
@@ -121,7 +121,7 @@ class FakeToolBar(QToolBar):
 
 class DockWidget(QDockWidget):
     def __init__(self, *args, **kwargs):
-        super(DockWidget, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         shortcuts = [
             QKeySequence(QKeySequence.Close),
             QKeySequence(QKeySequence(Qt.Key_Escape)),
@@ -1873,7 +1873,7 @@ class CanvasMainWindow(QMainWindow):
 
             return True
 
-        return QMainWindow.event(self, event)
+        return super().event(event)
 
     def show_help(self, url):
         """
@@ -1916,7 +1916,7 @@ class CanvasMainWindow(QMainWindow):
         """
         Reimplemented from QMainWindow.sizeHint
         """
-        hint = QMainWindow.sizeHint(self)
+        hint = super().sizeHint()
         return hint.expandedTo(QSize(1024, 720))
 
     def __update_from_settings(self):
@@ -2068,7 +2068,7 @@ class UrlDropEventFilter(QObject):
                 self.urlDropped.emit(url)
                 return True
 
-        return QObject.eventFilter(self, obj, event)
+        return super().eventFilter(obj, event)
 
 
 class RecentItem(SimpleNamespace):

orangecanvas/application/canvastooldock.py
@@ -33,7 +33,7 @@ class SplitterResizer(QObject):
     An object able to control the size of a widget in a QSplitter instance.
     """
     def __init__(self, parent=None):
-        QObject.__init__(self, parent)
+        super().__init__(parent)
         self.__splitter = None
         self.__widget = None
         self.__updateOnShow = True  # Need __update on next show event
@@ -202,7 +202,7 @@ class SplitterResizer(QObject):
             # Update the splitter state after receiving valid geometry
             self.__updateOnShow = False
             self.__update()
-        return QObject.eventFilter(self, obj, event)
+        return super().eventFilter(obj, event)
 
 
 class QuickHelpWidget(QuickHelp):
@@ -211,7 +211,7 @@ class QuickHelpWidget(QuickHelp):
         with a continuous animation.
 
         """
-        hint = QTextEdit.minimumSizeHint(self)
+        hint = super().minimumSizeHint()
         return QSize(hint.width(), 0)
 
 
@@ -221,7 +221,7 @@ class CanvasToolDock(QWidget):
 
     """
     def __init__(self, parent=None, **kwargs):
-        QWidget.__init__(self, parent, **kwargs)
+        super().__init__(parent, **kwargs)
 
         self.__setupUi()
 
@@ -277,8 +277,8 @@ class QuickCategoryToolbar(ToolGrid):
     """A toolbar with category buttons.
     """
     def __init__(self, parent=None, buttonSize=None, iconSize=None):
-        ToolGrid.__init__(self, parent, 1, buttonSize, iconSize,
-                          Qt.ToolButtonIconOnly)
+        super().__init__(parent, 1, buttonSize, iconSize,
+                         Qt.ToolButtonIconOnly)
         self.__model = None
 
     def setColumnCount(self, count):
@@ -320,7 +320,7 @@ class QuickCategoryToolbar(ToolGrid):
     def createButtonForAction(self, action):
         """Create a button for the action.
         """
-        button = ToolGrid.createButtonForAction(self, action)
+        button = super().createButtonForAction(action)
 
         item = qunwrap(action.data())  # QPersistentModelIndex
         assert isinstance(item, QPersistentModelIndex)
@@ -374,7 +374,7 @@ class CategoryPopupMenu(FramelessWindow):
     hovered = Signal(QAction)
 
     def __init__(self, parent=None, **kwargs):
-        FramelessWindow.__init__(self, parent, **kwargs)
+        super().__init__(parent, **kwargs)
         self.setWindowFlags(self.windowFlags() | Qt.Popup)
 
         layout = QVBoxLayout()
@@ -437,7 +437,7 @@ class CategoryPopupMenu(FramelessWindow):
         if self.__loop is not None:
             self.__loop.exit(0)
 
-        return FramelessWindow.hideEvent(self, event)
+        return super().hideEvent(event)
 
     def __onTriggered(self, action):
         self.__action = action
@@ -479,7 +479,7 @@ class ItemViewDragStartEventListener(QObject):
     dragStarted = Signal(QModelIndex)
 
     def __init__(self, parent=None):
-        QObject.__init__(self, parent)
+        super().__init__(parent)
         self._pos = None
         self._index = None
 
@@ -509,7 +509,7 @@ class ItemViewDragStartEventListener(QObject):
 
                 self.dragStarted.emit(index)
 
-        return QObject.eventFilter(self, view, event)
+        return super().eventFilter(view, event)
 
 
 class ToolTipEventFilter(QObject):
@@ -517,7 +517,7 @@ class ToolTipEventFilter(QObject):
         if event.type() == QEvent.ToolTip:
             return True
 
-        return QObject.eventFilter(self, receiver, event)
+        return super().eventFilter(receiver, event)
 
 
 def widget_popup_geometry(pos, widget):

orangecanvas/application/outputview.py
@@ -248,7 +248,7 @@ class ExceptHook(QObject):
     handledException = Signal(tuple)
 
     def __init__(self, parent=None, stream=None, **kwargs):
-        super(ExceptHook, self).__init__(parent, **kwargs)
+        super().__init__(parent, **kwargs)
         self.stream = stream
 
     def __call__(self, exc_type, exc_value, tb):

commit 1c7a7ebc280f4c212b860cb8291e4749151baae7

UserSettingsDialog: Fix a type error in super hideEvent call
Error introduced in 7d980fef00eeaca82ff10c2d68168c9f3ddad2e8

orangecanvas/application/settings.py
@@ -472,7 +472,7 @@ class UserSettingsDialog(QMainWindow):
         return status
 
     def hideEvent(self, event):
-        super().hideEvent(self, event)
+        super().hideEvent(event)
         if self.__loop is not None:
             self.__loop.exit(0)
             self.__loop = None