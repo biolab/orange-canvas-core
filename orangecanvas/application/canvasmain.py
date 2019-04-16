biolab/orange-canvas-core

commit 7854f50b0d77960f842821133bb70b3078e9c3a6

canvasmain: Copy the widget registry for own use

orangecanvas/application/canvasmain.py
@@ -59,7 +59,7 @@ from .outputview import OutputView, TextStream
 from .settings import UserSettingsDialog, category_state
 from ..document.schemeedit import SchemeEditWidget
 from ..gui.itemmodels import FilterProxyModel
-from ..registry import WidgetDescription, CategoryDescription
+from ..registry import WidgetRegistry, WidgetDescription, CategoryDescription
 from ..registry.qt import QtWidgetRegistry
 from ..utils.settings import QSettings_readArray, QSettings_writeArray
 
@@ -819,7 +819,13 @@ class CanvasMainWindow(QMainWindow):
         return self.__document_title
 
     def set_widget_registry(self, widget_registry):
-        """Set widget registry.
+        # type: (WidgetRegistry) -> None
+        """
+        Set widget registry.
+
+        Parameters
+        ----------
+        widget_registry : WidgetRegistry
         """
         if self.widget_registry is not None:
             # Clear the dock widget and popup.
@@ -830,18 +836,19 @@ class CanvasMainWindow(QMainWindow):
             self.__proxy_model.deleteLater()
             self.__proxy_model = None
 
-        self.widget_registry = widget_registry
+        self.widget_registry = WidgetRegistry(widget_registry)
+        qreg = QtWidgetRegistry(self.widget_registry, parent=self)
 
         # Restore category hidden/sort order state
         proxy = FilterProxyModel(self)
-        proxy.setSourceModel(widget_registry.model())
+        proxy.setSourceModel(qreg.model())
         self.__proxy_model = proxy
         self.__update_registry_filters()
 
         self.widgets_tool_box.setModel(proxy)
         self.quick_category.setModel(proxy)
 
-        self.scheme_widget.setRegistry(widget_registry)
+        self.scheme_widget.setRegistry(qreg)
         self.scheme_widget.quickMenu().setModel(proxy)
 
         self.help.set_registry(widget_registry)

commit 91032e0218b8eb95cae457312ffa0a8d96849c94

canvasmain: Fix type error in `on_quick_category_action`
Introduced in 7854f50b0d77960f842821133bb70b3078e9c3a6

orangecanvas/application/canvasmain.py
@@ -10,6 +10,7 @@ import io
 
 from functools import partial
 from types import SimpleNamespace
+from typing import Optional
 
 import pkg_resources
 
@@ -25,7 +26,7 @@ from AnyQt.QtGui import (
 
 from AnyQt.QtCore import (
     Qt, QObject, QEvent, QSize, QUrl, QTimer, QFile, QByteArray, QFileInfo,
-    QSettings, QStandardPaths, QT_VERSION
+    QSettings, QStandardPaths, QAbstractItemModel, QT_VERSION
 )
 
 try:
@@ -141,7 +142,8 @@ class CanvasMainWindow(QMainWindow):
         self.__document_title = "untitled"
         self.__first_show = True
         self.__is_transient = True
-        self.widget_registry = None
+        self.widget_registry = None  # type: Optional[WidgetRegistry]
+        self.__registry_model = None  # type: Optional[QAbstractItemModel]
         # Proxy widget registry model
         self.__proxy_model = None
 
@@ -838,7 +840,7 @@ class CanvasMainWindow(QMainWindow):
 
         self.widget_registry = WidgetRegistry(widget_registry)
         qreg = QtWidgetRegistry(self.widget_registry, parent=self)
-
+        self.__registry_model = qreg.model()
         # Restore category hidden/sort order state
         proxy = FilterProxyModel(self)
         proxy.setSourceModel(qreg.model())
@@ -888,11 +890,12 @@ class CanvasMainWindow(QMainWindow):
         if use_popover:
             # Show a popup menu with the widgets in the category
             popup = CategoryPopupMenu(self.quick_category)
-            reg = self.widget_registry.model()
+            model = self.__registry_model
+            assert model is not None
             i = index(self.widget_registry.categories(), category,
                       predicate=lambda name, cat: cat.name == name)
             if i != -1:
-                popup.setCategoryItem(reg.item(i))
+                popup.setCategoryItem(model.item(i))
                 button = self.quick_category.buttonForAction(action)
                 pos = popup_position_from_source(popup, button)
                 action = popup.exec_(pos)