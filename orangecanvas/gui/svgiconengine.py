bd230a23877b2d67b967b758fa9d796c864e509f
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-04-29 11:44:48 +0200

    SvgIconEngine: Use QPixmapCache for speedup

--- orangecanvas/gui/svgiconengine.py
+++ orangecanvas/gui/svgiconengine.py
@@ -1,22 +1,30 @@
+from itertools import count
+
 from AnyQt.QtCore import Qt, QSize, QRect, QRectF
-from AnyQt.QtGui import QIconEngine, QIcon, QPixmap, QPainter
+from AnyQt.QtGui import QIconEngine, QIcon, QPixmap, QPainter, QPixmapCache
 from AnyQt.QtSvg import QSvgRenderer
 from AnyQt.QtWidgets import QStyleOption, QApplication
+
+_cache_id_gen = count()
 
 
 class SvgIconEngine(QIconEngine):
     """
     An svg icon engine reimplementation drawing from in-memory svg contents.
+
     Arguments
     ---------
     contents : bytes
         The svg icon contents
     """
+    __slots__ = ("__contents", "__generator", "__cache_id")
+
     def __init__(self, contents):
         # type: (bytes) -> None
         super().__init__()
         self.__contents = contents
         self.__generator = QSvgRenderer(contents)
+        self.__cache_id = next(_cache_id_gen)
 
     def paint(self, painter, rect, mode, state):
         # type: (QPainter, QRect, QIcon.Mode, QIcon.State) -> None
@@ -40,15 +48,20 @@ class SvgIconEngine(QIconEngine):
         if not dsize.isNull():
             dsize.scale(size, Qt.KeepAspectRatio)
             size = dsize
-
-        pm = QPixmap(size)
-        pm.fill(Qt.transparent)
-        painter = QPainter(pm)
-        try:
-            self.__generator.render(
-                painter, QRectF(0, 0, size.width(), size.height()))
-        finally:
-            painter.end()
+        key = "{}.SVGIconEngine/{}/{}x{}".format(
+            __name__, self.__cache_id, size.width(), size.height()
+        )
+        pm = QPixmapCache.find(key)
+        if pm is None or pm.isNull():
+            pm = QPixmap(size)
+            pm.fill(Qt.transparent)
+            painter = QPainter(pm)
+            try:
+                self.__generator.render(
+                    painter, QRectF(0, 0, size.width(), size.height()))
+            finally:
+                painter.end()
+            QPixmapCache.insert(key, pm)
         style = QApplication.style()
         if style is not None:
             opt = QStyleOption()
