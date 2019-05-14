cc50a945c44efc8ab0142fcdc87e9c94b2e51a03
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-05-14 15:53:04 +0200

    tests: Set QStandardPaths.setTestModeEnabled in tests
    Also set/restore application name and domain

--- orangecanvas/gui/test.py
+++ orangecanvas/gui/test.py
@@ -7,20 +7,28 @@ import unittest
 import gc
 
 from AnyQt.QtWidgets import QApplication
-from AnyQt.QtCore import QCoreApplication, QTimer
+from AnyQt.QtCore import QCoreApplication, QTimer, QStandardPaths
 from AnyQt.QtTest import QTest
 
 
 class QCoreAppTestCase(unittest.TestCase):
     _AppClass = QCoreApplication
 
+    __appdomain = ""
+    __appname = ""
+
     @classmethod
     def setUpClass(cls):
         super(QCoreAppTestCase, cls).setUpClass()
+        QStandardPaths.setTestModeEnabled(True)
         app = cls._AppClass.instance()
         if app is None:
             app = cls._AppClass([])
         cls.app = app
+        cls.__appname = cls.app.applicationName()
+        cls.__appdomain = cls.app.organizationDomain()
+        cls.app.setApplicationName("orangecanvas.testing")
+        cls.app.setOrganizationDomain("biolab.si")
 
     def setUp(self):
         super(QCoreAppTestCase, self).setUp()
@@ -37,8 +45,11 @@ class QCoreAppTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         gc.collect()
+        cls.app.setApplicationName(cls.__appname)
+        cls.app.setOrganizationDomain(cls.__appdomain)
         cls.app = None
         super(QCoreAppTestCase, cls).tearDownClass()
+        QStandardPaths.setTestModeEnabled(False)
 
 
 class QAppTestCase(QCoreAppTestCase):
