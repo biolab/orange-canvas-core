1f10f641efdbaf892c2a0d2f73d4f47ca1e2336a
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-07-25 09:35:27 +0200

    Bump version to 0.1.5.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 NAME = "orange-canvas-core"
-VERSION = "0.1.4"
+VERSION = "0.1.5.dev0"
 DESCRIPTION = "Core component of Orange Canvas"
 
 with open("README.rst", "rt", encoding="utf-8") as f:
