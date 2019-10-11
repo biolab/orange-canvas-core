c5a23a26d5cf7d4b8b114febcdddfc524a974d94
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-10-11 16:46:55 +0200

    widgetmanager: Re dispatch (Input|Output)LinkStateChange events

--- orangecanvas/scheme/widgetmanager.py
+++ orangecanvas/scheme/widgetmanager.py
@@ -53,12 +53,16 @@ class WidgetManager(QObject):
     The widgets created with :func:`create_widget_for_node` will automatically
     receive dispatched events:
 
-        * :data:`WorkflowEvent.InputLinkAdded` - when a new input link is added to
-          the workflow.
-        * :data:`LinkEvent.InputLinkRemoved` - when a input link is removed
-        * :data:`LinkEvent.OutputLinkAdded` - when a new output link is added to
-          the workflow
-        * :data:`LinkEvent.InputLinkRemoved` - when a output link is removed
+        * :data:`WorkflowEvent.InputLinkAdded` - when a new input link is
+          added to the workflow.
+        * :data:`LinkEvent.InputLinkRemoved` - when a input link is removed.
+        * :data:`LinkEvent.OutputLinkAdded` - when a new output link is
+          added to the workflow.
+        * :data:`LinkEvent.InputLinkRemoved` - when a output link is removed.
+        * :data:`LinkEvent.InputLinkStateChanged` - when the input link's
+          runtime state changes.
+        * :data:`LinkEvent.OutputLinkStateChanged` - when the output link's
+          runtime state changes.
         * :data:`WorkflowEnvEvent.WorkflowEnvironmentChanged` - when the
           workflow environment changes.
 
@@ -588,9 +592,17 @@ class WidgetManager(QObject):
 
     def eventFilter(self, recv, event):
         # type: (QObject, QEvent) -> bool
-        if event.type() == NodeEvent.NodeActivateRequest \
-                and isinstance(recv, SchemeNode):
-            self.__activate_widget_for_node(recv)
+        if isinstance(recv, SchemeNode):
+            if event.type() == NodeEvent.NodeActivateRequest:
+                self.__activate_widget_for_node(recv)
+            elif event.type() in (
+                    LinkEvent.InputLinkStateChange,
+                    LinkEvent.OutputLinkStateChange,
+            ):
+                item = self.__item_for_node.get(recv)
+                # dispatch the event to the gui widget
+                if item is not None and item.widget is not None:
+                    QCoreApplication.sendEvent(item.widget, event)
         return False
 
     def __set_float_on_top_flag(self, widget):
