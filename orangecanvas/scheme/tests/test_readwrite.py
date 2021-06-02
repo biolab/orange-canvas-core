d81c07a7147a9e50cf6941964ef95acebd693605
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-06-02 15:42:51 +0200

    readwrite: Macro nodes

--- orangecanvas/scheme/tests/test_readwrite.py
+++ orangecanvas/scheme/tests/test_readwrite.py
@@ -10,7 +10,7 @@ from ...registry import WidgetRegistry, WidgetDescription, CategoryDescription, 
 from ...registry import tests as registry_tests
 
 from .. import Scheme, SchemeNode, SchemeLink, \
-               SchemeArrowAnnotation, SchemeTextAnnotation
+    SchemeArrowAnnotation, SchemeTextAnnotation, MetaNode
 
 from .. import readwrite
 
@@ -170,6 +170,77 @@ class TestReadWrite(test.QAppTestCase):
         self.assertEqual(node_1.input_channels()[-1].name, "b")
         self.assertEqual(node_1.output_channels()[-1].name, "b")
 
+    def test_macro(self):
+        tree = ET.parse(io.BytesIO(FOOBAR_v30.encode()))
+        root = readwrite.parse_ows_etree_v_3_0(tree)
+        self.assertEqual(len(root.nodes), 3)
+        self.assertEqual(len(root.links), 2)
+        macro = root.nodes[2]
+        self.assertIsInstance(macro, readwrite._macro_node)
+        self.assertEqual(len(macro.nodes), 3)
+        self.assertEqual(len(macro.links), 2)
+        self.assertEqual(len(macro.annotations), 1)
+
+        workflow = Scheme()
+        reg = foo_registry()
+        workflow.load_from(io.BytesIO(FOOBAR_v30.encode()), registry=reg)
+        root = workflow.root()
+        macro = root.nodes()[2]
+        self.assertIsInstance(macro, MetaNode)
+        self.assertEqual(len(macro.nodes()), 3)
+        self.assertEqual(len(macro.links()), 2)
+        self.assertEqual(len(macro.annotations()), 1)
+
+        stream = io.BytesIO()
+        workflow.save_to(stream, )
+        workflow.clear()
+        stream.seek(0)
+        workflow.load_from(stream, registry=reg)
+
+        root = workflow.root()
+        macro = root.nodes()[2]
+        self.assertIsInstance(macro, MetaNode)
+        self.assertEqual(len(macro.nodes()), 3)
+        self.assertEqual(len(macro.links()), 2)
+        self.assertEqual(len(macro.annotations()), 1)
+
+    def test_properties_restore(self):
+        def load(stream):
+            wf = Scheme()
+            reg = foo_registry()
+            readwrite.scheme_load(wf, stream, reg)
+            return wf
+
+        workflow = load(io.BytesIO(FOOBAR_v30.encode()))
+        root = workflow.root()
+        nodes = root.nodes()
+        n1, n2 = nodes[0], nodes[1]
+        self.assertEqual(n1.properties, {"id": 1})
+        self.assertEqual(n2.properties, {"id": 2})
+        meta = nodes[2]
+        assert isinstance(meta, MetaNode)
+        n6 = meta.nodes()[2]
+        assert n6.properties == {"id": 6}
+
+        n6.properties = {"id": 6, "x": "1"}
+        n2.properties = {"id": 2, "x": "2"}
+        n1.properties = {"id": 1, "x": "3"}
+
+        buffer = io.BytesIO()
+        readwrite.scheme_to_ows_stream(workflow, buffer, pretty=True)
+        buffer.seek(0)
+        workflow = load(buffer)
+
+        root = workflow.root()
+        nodes = root.nodes()
+        n1, n2 = nodes[0], nodes[1]
+        self.assertEqual(n1.properties, {"id": 1, "x": "3"})
+        self.assertEqual(n2.properties, {"id": 2, "x": "2"})
+        meta = nodes[2]
+        assert isinstance(meta, MetaNode)
+        n6 = meta.nodes()[2]
+        self.assertEqual(n6.properties, {"id": 6, "x": "1"})
+
 
 def foo_registry():
     reg = WidgetRegistry()
@@ -181,6 +252,8 @@ def foo_registry():
             qualified_name="package.foo",
             project_name="Foo",
             category="Quack",
+            inputs=[InputSignal("foo", object,)],
+            outputs=[OutputSignal("foo", object)],
         )
     )
     reg.register_widget(
@@ -191,26 +264,12 @@ def foo_registry():
             project_name="Bar",
             replaces=["package.bar"],
             category="Quack",
+            inputs=[InputSignal("bar", object, )],
+            outputs=[OutputSignal("bar", object)],
         )
     )
     return reg
 
-
-FOOBAR_v10 = """<?xml version="1.0" ?>
-<schema>
-    <widgets>
-        <widget caption="Foo" widgetName="foo" xPos="1" yPos="2"/>
-        <widget caption="Bar" widgetName="bar" xPos="2" yPos="3"/>
-    </widgets>
-    <channels>
-        <channel enabled="1" inWidgetCaption="Foo" outWidgetCaption="Bar"
-                 signals="[('foo', 'bar')]"/>
-        <channel enabled="0" inWidgetCaption="Foo" outWidgetCaption="Bar"
-                 signals="[('foo1', 'bar1')]"/>
-    </channels>
-    <settings settingsDictionary="{}"/>
-</schema>
-"""
 
 FOOBAR_v20 = """<?xml version="1.0" ?>
 <scheme title="FooBar" description="Foo to the bar" version="2.0">
@@ -226,5 +285,46 @@ FOOBAR_v20 = """<?xml version="1.0" ?>
         <link enabled="false" id="1" sink_channel="bar1" sink_node_id="1"
               source_channel="foo1" source_node_id="0" />
     </links>
+</scheme>
+"""
+
+FOOBAR_v30 = """<?xml version="1.0" ?>
+<scheme title="FooBar" description="Foo to the bar" version="3.0">
+    <nodes>
+        <node id="1" title="Foo" position="1, 2" project_name="Foo"
+              qualified_name="package.foo" />
+        <node id="2" title="Bar" position="2, 3" project_name="Foo"
+              qualified_name="package.bar" />
+        <macro_node id="3" title="Frobnicate" position="3, 3" >
+            <nodes>
+                <input_node id="4" title="In" type="object" position="1, 1" />
+                <output_node id="5" title="Out" type="object" position="1, 2" />
+                <node id="6" title="Baz" position="1, 3" project_name="Foo"
+                      qualified_name="package.bar" />
+            </nodes>
+            <links>
+                <link id="1" enabled="true"
+                      source_node_id="4" source_channel="In"
+                      sink_node_id="6" sink_channel="bar" />
+                <link id="2" enabled="true"
+                      source_node_id="6" source_channel="bar"
+                      sink_node_id="5" sink_channel="Out" />
+            </links>
+            <annotations>
+                <text geometry="1, 1, 3, 3" >This is a Baz</text>
+            </annotations>
+        </macro_node>
+    </nodes>
+    <links>
+        <link id="3" enabled="true" sink_channel="In" sink_node_id="3"
+              source_channel="foo" source_node_id="1" />
+        <link id="4" enabled="true" sink_channel="bar" sink_node_id="2"
+              source_channel="Out" source_node_id="3" />
+    </links>
+    <node_properties>
+        <properties node_id="1" format="literal">{"id": 1}</properties>
+        <properties node_id="2" format="literal">{"id": 2}</properties>
+        <properties node_id="6" format="literal">{"id": 6}</properties>
+    </node_properties>
 </scheme>
 """
