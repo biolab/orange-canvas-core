47e9cc9710bfa031cec776a662fed601848af705
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-08-06 11:12:55 +0200

    commands: Root meta

--- orangecanvas/document/interactions.py
+++ orangecanvas/document/interactions.py
@@ -668,7 +668,7 @@ class NewLinkAction(UserInteraction):
                     node = None
 
                 if node is not None:
-                    commands.AddNodeCommand(self.scheme, node,
+                    commands.AddNodeCommand(self.scheme, node, self.scheme.root(),
                                             parent=self.macro)
 
             if node is not None and not self.showing_incompatible_widget:
@@ -788,6 +788,7 @@ class NewLinkAction(UserInteraction):
         detailed dialog for link editing.
 
         """
+        root = self.scheme.root()
         UsageStatistics.set_sink_anchor_open(sink_signal is not None)
         UsageStatistics.set_source_anchor_open(source_signal is not None)
         try:
@@ -855,7 +856,7 @@ class NewLinkAction(UserInteraction):
             self.cleanup()
 
             for link in links_to_remove:
-                commands.RemoveLinkCommand(self.scheme, link,
+                commands.RemoveLinkCommand(self.scheme, link, root,
                                            parent=self.macro)
 
             for link in links_to_add:
@@ -867,7 +868,7 @@ class NewLinkAction(UserInteraction):
                 )
 
                 if not duplicate:
-                    commands.AddLinkCommand(self.scheme, link,
+                    commands.AddLinkCommand(self.scheme, link, root,
                                             parent=self.macro)
 
         except scheme.IncompatibleChannelTypeError:
